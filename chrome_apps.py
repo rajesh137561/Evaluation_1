3305cde954a63e0fefffbf20136d8c4e74980ba3
rajesh137561 <106002622+rajesh137561@users.noreply.github.com>
2022-05-22 19:27:07 +0530

    Add files via upload

---
+++ chrome_apps.py
@@ -0,0 +1,107 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun May 22 09:41:19 2022
+
+@author: Rajesh
+"""
+
+
+import pandas as pd
+
+df = pd.read_csv("C:/Users/HP/Desktop/chrome_reviews.csv")
+
+import numpy as np
+df_NA = df.dropna(how = 'all')
+#df[df['Star'] != 3]
+#df_NA.keys()
+df_NA['Positivity'] = np.where(df_NA['Star'] > 3, 1, 0)
+cols = ['ID', 'Star', 'Review URL', 'Thumbs Up', 'User Name', 'Developer Reply', 'Version','Review Date', 'App ID']
+df_NA.drop(cols, axis=1, inplace=True)
+
+import re
+import nltk
+nltk.download('stopwords')
+from nltk.corpus import stopwords
+from nltk.stem.porter import PorterStemmer
+
+
+
+# Initialize empty array
+# to append clean text
+corpus = []
+num = len(df_NA)
+
+df_NA['Text'] = df_NA['Text'].astype(str)
+# (reviews) rows to clean
+for i in range(0, num):
+	
+	# column : "Text", row ith
+	review = re.sub('[^a-zA-Z]', ' ', df_NA['Text'][i])
+	
+	# convert all cases to lower cases
+	review = review.lower()
+	
+	# split to array(default delimiter is " ")
+	review = review.split()
+	
+	# creating PorterStemmer object to
+	# take main stem of each word
+	ps = PorterStemmer()
+	
+	# loop for stemming each word
+	# in string array at ith row
+	review = [ps.stem(word) for word in review
+				if not word in set(stopwords.words('english'))]
+				
+	# rejoin all string array elements
+	# to create back into a string
+	review = ' '.join(review)
+	
+	# append each string to create
+	# array of clean text
+	corpus.append(review)
+
+from sklearn.feature_extraction.text import CountVectorizer
+cv = CountVectorizer(max_features = 1000)
+X = cv.fit_transform(corpus).toarray()
+y = df_NA.iloc[:, 1].values
+
+# train a random forest model to perform classification as positive or negative review
+from sklearn.model_selection import train_test_split
+x_train, x_cv, y_train, y_cv = train_test_split(X, y, test_size = 0.20)
+from sklearn.ensemble import RandomForestClassifier
+model = RandomForestClassifier(n_estimators = 350,criterion = 'entropy')
+                             
+model.fit(x_train, y_train)
+
+from sklearn.metrics import accuracy_score
+pred_cv = model.predict(x_cv)
+accuracy_score(y_cv,pred_cv)
+
+pred_train = model.predict(x_train)
+accuracy_score(y_train,pred_train)
+
+import pickle 
+pickle_out = open("classifier.pkl", mode = "wb") 
+pickle.dump(model, pickle_out) 
+pickle_out.close()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
