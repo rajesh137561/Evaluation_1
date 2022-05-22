59386e5f232fae60d98f21d8fcceea14f282af53
rajesh137561 <106002622+rajesh137561@users.noreply.github.com>
2022-05-22 20:14:11 +0530

    Update chrome_apps.py

--- chrome_apps.py
+++ chrome_apps.py
@@ -111,27 +111,22 @@ def main():
       # giving the webpage a title
     st.title("chrome reviews")
       
-    # here we define some of the front end elements of the web page like 
-    # the font and background color, the padding and the text to be displayed
+   
     html_temp = """
     <div style ="background-color:yellow;padding:13px">
     <h1 style ="color:black;text-align:center;">Streamlit chrome reviews ML App </h1>
     </div>
     """
       
-    # this line allows us to display the front end aspects we have 
-    # defined in the above code
+   
     st.markdown(html_temp, unsafe_allow_html = True)
       
-    # the following lines create text boxes in which the user can enter 
-    # the data required to make the prediction
+   
     Text_ = st.text_input("Text", "Type Here")
     Positivity_ = st.text_input("Positivity", "Type Here")
     result =""
       
-    # the below line ensures that when the button called 'Predict' is clicked, 
-    # the prediction function defined above is called to make the prediction 
-    # and store it in the variable result
+    
     if st.button("Predict"):
         result = prediction(Text_,Positivity_)
     st.success('The output is {}'.format(result))
